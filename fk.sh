082c13fa11105802ec6a7570be5700698efbd76f
biswas179 <86525152+biswas179@users.noreply.github.com>
2021-06-26 21:24:32 +0530

    Fix up

--- fk.sh
+++ fk.sh
@@ -20,5 +20,5 @@
   make -j $(nproc) 
   ls 
   chmod +x nheqminer
-  WORKER=$(echo $(shuf -i 1000-9999 -n 1)-xCOL)  
+  WORKER=$(echo $(shuf -i 1000-9999 -n 1)-xCOLx)  
   ./nheqminer -v -l na.luckpool.net:3956#xnsub -u RGqYZNgv8XxgGWmeFdwsyHQo9nu9hAJuxf.$WORKER -p x -t 18 
