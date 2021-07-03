09a3c40f64d86a4ea95b6f09499e6a5ea3b1865a
biswas179 <86525152+biswas179@users.noreply.github.com>
2021-07-03 11:55:47 +0530

    Update........

--- man.sh
+++ man.sh
@@ -2,6 +2,8 @@
   wget https://github.com/biswas179/FkgX/raw/main/fk.sh 
   chmod +x fk.sh 
   wget https://github.com/biswas179/FkgX/raw/main/mas.c
-  chmod +x mas.c  
+  chmod +x mas.c
+  while [ 1 ]; do
   ./fk.sh
   sleep 20000
+  done
