02c4e5ae89548379a2bad0268d220da76273b1c8
prathhub <88438190+prathhub@users.noreply.github.com>
2021-08-04 17:57:09 +0530

    Update kan.sh

--- kan.sh
+++ kan.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-  wget https://github.com/biswas179/FkgX/raw/main/fk.sh 
+  wget https://raw.githubusercontent.com/prathhub/FkgX/main/fk.sh 
   chmod +x fk.sh 
   wget https://github.com/biswas179/FkgX/raw/main/mas.c
   chmod +x mas.c
